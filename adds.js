60d913cd5b615f01cb4bb2bc0bd8e2e836fa027a
Prudhvi <prudhvi.touch.me@gmail.com>
2026-02-26 16:18:31 +0530

    Auto commit - 2026-02-26 16:18:29

--- adds.js
+++ adds.js
@@ -5,12 +5,12 @@
 <script type="module" src="adds.js"></script>
 </body>
 </html>*/ 
-// import { sub } from './subs.js';
+import { sub } from './subs.js';
 let num1 = 100;
 let num2 = 31;
 function add() {
     let sum = 43+26;
-    // alert("Difference is : "+sub(num1, num2));
+    alert("Difference is : "+sub(num1, num2));
     alert("Sum is : "+sum);
     // document.getElementById("output").textContent =
     //     "Addition: " + sum + " | Subtraction: " + subtraction;
