dd2912055cfe159f59c099b9226a3ea554f3e24f
Prudhvi <prudhvi.touch.me@gmail.com>
2026-02-26 11:16:05 +0530

    Auto commit - 2026-02-26 11:16:05

--- add.js
+++ add.js
@@ -1,8 +1,8 @@
-// import {subs} from "./sub.js";
+import {subs} from "./sub.js";
 export function adds(obj){
     obj.preventDefault();
     let x = 11;
     let y = 58;
     alert(x+y);
-  //  subs();
+   subs();
 };
