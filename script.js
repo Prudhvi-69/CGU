228c688d11d2b31d6fac2bd428152c982e655165
Prudhvi <prudhvi.touch.me@gmail.com>
2026-02-26 10:36:23 +0530

    Auto commit - 2026-02-26 10:36:23

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('btn').addEventListener('click', function(obj){
+            obj.preventDefault();
+        document.getElementById('content').innerHTML = "Form submitted without reload";
+        });
+        document.getElementById('btn2').addEventListener('click', function(cgu){
+           //cgu.preventDefault(); //stops reload
+            document.getElementById('content').innerHTML = "Form reset without reload";
+        });
+        document.getElementById('refresh').addEventListener('click', function(){
+            location.reload();
+        });
