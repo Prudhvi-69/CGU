af84e42a00633e298fd988fbc11fb4869e1cf643
Prudhvi <prudhvi.touch.me@gmail.com>
2026-02-26 16:11:17 +0530

    Auto commit - 2026-02-26 16:11:17

--- file1.js
+++ file1.js
@@ -1,12 +1,11 @@
-function add(){
-   
+function add(obj){
         obj.preventDefault(); //It stops the page from reloading when the button is clicked
         let t = document.getElementById('val1').value;
         document.getElementById('result').innerHTML="Input Entering is: "+t;
-    });
-    document.getElementById('refresh').addEventListener('click', function refresh(){
+}
+function refresh()
+{
         confirm("Are you sure you want to refresh the page? All data will be lost.");
         //this function will give us two options of okay or cancel unlike alert only okay
        location.reload(); //Refresh page manually with a button
-    });
 }
