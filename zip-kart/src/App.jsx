8d64a1330509cf7ef2896bfb862a76f5228a97b2
Prudhvi <prudhvi.touch.me@gmail.com>
2026-03-13 13:41:10 +0530

    S2 Fetch function code push

--- zip-kart/src/App.jsx
+++ zip-kart/src/App.jsx
@@ -13,61 +13,86 @@ import { ErrorPage } from './components/ErrorPage'
 import Register from './components/Register'
 import Login from './components/Login'
 import StudentForm from './pages/StudentForm'
+import { useState,useEffect } from 'react'
 function App() {
+  const [users,setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(result => result.json())
+    .then(data => setUsers(data));
+  },[]);
   return (
-  <BrowserRouter>
-    <nav>
-      <NavLink to="/" style={({isActive}) => ({
-        color: isActive ? "blue" : "hotpink"
-      })}>Home </NavLink> | 
-      <NavLink to="/reviews" style={({isActive}) => ({
-        color: isActive ? "skyblue" : "hotpink"
-      })}>Reviews</NavLink> |
-      <NavLink to="/services" style={({isActive}) => ({
-        color: isActive ? "orange" : "hotpink"
-      })}>Services </NavLink> | 
-      <NavLink to="/about" style={({isActive}) => ({
-        color: isActive ? "green" : "hotpink"
-      })}>About</NavLink> |
-      <NavLink to="/contactus" style={({isActive}) => ({
-        color: isActive ? "white" : "hotpink"
-      })}>Contact Us </NavLink> | 
-      <NavLink to="/login" style={({isActive}) => ({
-        color: isActive ? "red" : "hotpink"
-      })}>Login</NavLink>  | 
-      <NavLink to="/register"style={({isActive}) => ({
-        color: isActive ? "teal" : "hotpink"
-      })}>Register</NavLink> 
-      <NavLink to="/studentform" style={({isActive}) => ({
-        color: isActive ? "purple" : "hotpink"
-      })}>Student Form</NavLink>
-    </nav>
-    {/* <nav>
-      <Link to="/">Home</Link>  |  
-      <Link to="/reviews">Reviews</Link>  |  
-      <Link to="/services">Services</Link>  |  
-      <Link to="/about">About</Link>  |  
-      <Link to="/contactus">Contact Us</Link>  |  
-      <Link to="/login">Login</Link>
-    </nav> */}
-    <Routes>
-      <Route path="/" element={<HomePage/> } />
-      <Route path="/reviews" element={<Reviews/>} />
-      <Route path="/services" element={<h1>This is Services Page</h1>} />
-      <Route path="/about" element={<h1>About Page</h1>} />
-      <Route path="/contactus" element={<Contactus/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="*" element={<ErrorPage/>}/>
-      <Route path="/register" element={<Register name="CGU"/>}/>
-      <Route path="/success" element={<h1>Login Successful</h1>}/>
-      <Route path="/studentform" element={<StudentForm/>}/>
-    </Routes>
-        {/* <Counts name="Hundia"/> */}
-  </BrowserRouter>
+    <>
+    <h1>Data fetching from JSON Placeholder</h1>
+    {users.map(user => (
+      <div>
+  <p key={user.id}>Name : {user.name}</p>
+  <p key={user.id}>Email : {user.email}</p>
+  <p key={user.id}>Phone : {user.phone}</p>
+  <p key={user.id}>Website : {user.website}</p>      
+      </div>
+  ))}
+  </>
+  // <>
+  // <BrowserRouter>
+  //   <nav>
+  //     <NavLink to="/" style={({isActive}) => ({
+  //       color: isActive ? "blue" : "hotpink"
+  //     })}>Home </NavLink> | 
+  //     <NavLink to="/reviews" style={({isActive}) => ({
+  //       color: isActive ? "skyblue" : "hotpink"
+  //     })}>Reviews</NavLink> |
+  //     <NavLink to="/services" style={({isActive}) => ({
+  //       color: isActive ? "orange" : "hotpink"
+  //     })}>Services </NavLink> | 
+  //     <NavLink to="/about" style={({isActive}) => ({
+  //       color: isActive ? "green" : "hotpink"
+  //     })}>About</NavLink> |
+  //     <NavLink to="/contactus" style={({isActive}) => ({
+  //       color: isActive ? "white" : "hotpink"
+  //     })}>Contact Us </NavLink> | 
+  //     <NavLink to="/login" style={({isActive}) => ({
+  //       color: isActive ? "red" : "hotpink"
+  //     })}>Login</NavLink>  | 
+  //     <NavLink to="/register"style={({isActive}) => ({
+  //       color: isActive ? "teal" : "hotpink"
+  //     })}>Register</NavLink> 
+  //     <NavLink to="/studentform" style={({isActive}) => ({
+  //       color: isActive ? "purple" : "hotpink"
+  //     })}>Student Form</NavLink>
+  //   </nav>
+  //   {/* <nav>
+  //     <Link to="/">Home</Link>  |  
+  //     <Link to="/reviews">Reviews</Link>  |  
+  //     <Link to="/services">Services</Link>  |  
+  //     <Link to="/about">About</Link>  |  
+  //     <Link to="/contactus">Contact Us</Link>  |  
+  //     <Link to="/login">Login</Link>
+  //   </nav> */}
+  //   <Routes>
+  //     <Route path="/" element={<HomePage/> } />
+  //     <Route path="/reviews" element={<Reviews/>} />
+  //     <Route path="/services" element={<h1>This is Services Page</h1>} />
+  //     <Route path="/about" element={<h1>About Page</h1>} />
+  //     <Route path="/contactus" element={<Contactus/>} />
+  //     <Route path="/login" element={<Login/>} />
+  //     <Route path="*" element={<ErrorPage/>}/>
+  //     <Route path="/register" element={<Register name="CGU"/>}/>
+  //     <Route path="/success" element={<h1>Login Successful</h1>}/>
+  //     <Route path="/studentform" element={<StudentForm/>}/>
+  //   </Routes>
+  //       {/* <Counts name="Hundia"/> */}
+  // </BrowserRouter>
 
 
-    // {/* <Counts name="CGU" age="69"/>
-    // <Counter/> */}
+  //   {/* <Counts name="CGU" age="69"/>
+  //   <Counter/> */}
+    
+  //   <h1> Users Data </h1>
+  //   {users.map(user => (
+  //       <p key={user.id}>{user.name}</p>
+  // ))}
+  //   </>
   )
 }
 export default App
